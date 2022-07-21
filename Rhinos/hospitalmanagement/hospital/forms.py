9c3d4ffefef7506a7d23108e842d358f66bf0b9f
Hieu0901VAG <92003038+Hieu0901VAG@users.noreply.github.com>
2022-07-21 10:57:02 +0700

    Update forms.py
    edit chú thích

--- Rhinos/hospitalmanagement/hospital/forms.py
+++ Rhinos/hospitalmanagement/hospital/forms.py
@@ -14,7 +14,7 @@ class AdminSigupForm(forms.ModelForm):
         }
 
 
-#for student related form
+#for doctor related form
 class DoctorUserForm(forms.ModelForm):
     class Meta:
         model=User
@@ -29,7 +29,7 @@ class DoctorForm(forms.ModelForm):
 
 
 
-#for teacher related form
+#for patient related form
 class PatientUserForm(forms.ModelForm):
     class Meta:
         model=User
